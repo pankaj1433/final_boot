276dea55eb4c3bad407d648f194aa957c1dc75c4
pankaj1433 <pankaj.007n@gmail.com>
2017-02-24 10:18:45 +0530

    final

---
+++ assement 1/javascript/app.js
@@ -0,0 +1,32 @@
+(function() {
+  'use strict';
+
+  // Do not write anything above this line and do not delete it
+
+
+  // This is our note pad for testing JS
+  // Write your code here
+
+//  window.alert('Welcome interns');
+  console.log('We will use console api for logging purposes');
+
+
+  // Do not write anything below this line and do not delete it
+/*
+  var primitive_undefined;
+  console.log('Every declared uninitialized variable holds the value : ', primitive_undefined);
+
+
+  console.log('If we try access a variable that is not declared, we get : ');
+  console.log(undeclared_variable);
+*/
+/*
+var null_value = null;
+console.log(null_value, 'represents intentional absense of Object value');
+null_value = new Date();*/
+
+var a =[1,44,2,60,3,5,6,12,234,35];
+  console.log(function (x,y){return a-b;});
+
+
+}());
